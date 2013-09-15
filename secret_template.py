14d84dbe9229a47d48f4fb07968634a8d803a7cc
jabel <james.c.abel@intel.com>
2013-09-14 19:15:54 -0700

    added the secret template file

---
+++ secret_template.py
@@ -0,0 +1,13 @@
+
+# fill this in with real values and rename the file to secret.py
+
+# Real applications authenticate with Evernote using OAuth, but for the
+# purpose of exploring the API, you can get a developer token that allows
+# you to access your own Evernote account. To get a developer token, visit
+# https://sandbox.evernote.com/api/DeveloperToken.action
+
+auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+consumer_key = "xxxxxxxxxxx"
+consumer_secret = "xxxxxxxxxxxxxxxx"
+
